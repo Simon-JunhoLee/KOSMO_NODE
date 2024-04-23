c8e096d52c7e1b392aa7d5aba76e0922eb4f1382
Simon-JunhoLee <leejunho0722@naver.com>
2024-04-23 17:55:58 +0900

    ver.02

--- ex02/routes/local.js
+++ ex02/routes/local.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* 지역검색. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '지역검색', pageName : 'home.ejs'});
+  res.render('index', { title: '지역검색', pageName : 'local/search.ejs'});
 });
 
 module.exports = router;
