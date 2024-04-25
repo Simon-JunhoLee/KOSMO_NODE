3ec3773d8b852323d114fc01585f24f8e642f4ae
Simon-JunhoLee <leejunho0722@naver.com>
2024-04-25 11:57:37 +0900

    ver.04

--- ex04/routes/book.js
+++ ex04/routes/book.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET 도서검색 page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'KOSMO', pageName : 'home.ejs'});
+  res.render('index', { title: '도서검색', pageName : 'book/search.ejs'});
 });
 
 module.exports = router;
--- ex04/routes/local.js
+++ ex04/routes/local.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* 지역검색. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'KOSMO', pageName : 'home.ejs'});
+  res.render('index', { title: '지역검색', pageName : 'local/search.ejs'});
 });
 
 module.exports = router;
