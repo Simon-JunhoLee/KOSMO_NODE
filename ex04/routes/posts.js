602724e33f0107af8e318cd8f2527cab9a5b5484
Simon-JunhoLee <leejunho0722@naver.com>
2024-04-25 18:07:34 +0900

    ver.04

---
+++ ex04/routes/posts.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../db');
+
+/* 게시판 목록 페이지 */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '게시판', pageName : 'posts/list.ejs'});
+});
+
+// 게시판 목록 데이터 불러오기
+router.get('/list.json', function(req, res){
+    const sql = "SELECT pid, title, contents, writer, DATE_FORMAT(pdate, '%y.%m.%d. %r') AS formatted_pdate FROM posts order by pid desc";
+    db.get().query(sql, function(err, rows){
+        if(err){
+            console.log("게시판 목록 : ", err);
+        }else {
+            res.send(rows);
+        }
+    });
+});
+
+// 글쓰기 페이지로 이동
+router.get('/insert', function(req, res, next) {
+    res.render('index', { title: '글쓰기', pageName : 'posts/insert.ejs'});
+});
+
+router.post('/insert', function(req, res){
+    const title = req.body.title;
+    const contents = req.body.contents;
+    const uid = req.body.uid;
+    console.log(title, contents, uid);
+    const sql = "insert into posts(title, contents, writer) values(?, ?, ?)";
+    db.get().query(sql, [title, contents, uid], function(err, rows){
+        if(err){
+            console.log("게시글 작성 오류 : ", err);
+        }else {
+            res.redirect('/posts');
+        }
+    })
+});
+
+// 게시글 Read 페이지 이동
+router.get('/read', function(req, res, next) {
+    const pid = req.query.pid;
+    console.log(pid);
+    const sql = "select pid, title, contents, writer, DATE_FORMAT(pdate, '%y.%m.%d. %r') AS formatted_pdate from posts where pid=?";
+    db.get().query(sql,[pid], function(err, rows){
+        if(err){
+            console.log("게시글 상세페이지 오류 : ", err)
+        }else {
+            console.log(rows[0]);
+            res.render('index', { title: '게시글 정보', pageName : 'posts/read.ejs', post : rows[0]});
+        }
+    });
+});
+
+module.exports = router;
